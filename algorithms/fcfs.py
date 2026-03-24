5f2300d2d54eb177b52b99cb072c6e2b6ab3fba2
AZIM SADATH <azimsadath521@gmail.com>
2026-03-24 17:14:48 +0530

    Implemented FCFS scheduling algorithm

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -1,1 +1,39 @@
-# FCFS scheduling algorithm placeholder
+def fcfs_scheduling(processes):
+    """
+    First Come First Serve Scheduling Algorithm
+    """
+
+    processes = sorted(processes, key=lambda x: x["arrival"])
+
+    current_time = 0
+    results = []
+    timeline = []
+
+    for process in processes:
+
+        pid = process["pid"]
+        arrival = process["arrival"]
+        burst = process["burst"]
+
+        if current_time < arrival:
+            current_time = arrival
+
+        start_time = current_time
+        finish_time = start_time + burst
+
+        waiting_time = start_time - arrival
+        turnaround_time = finish_time - arrival
+
+        results.append({
+            "pid": pid,
+            "arrival": arrival,
+            "burst": burst,
+            "waiting_time": waiting_time,
+            "turnaround_time": turnaround_time
+        })
+
+        timeline.append((pid, start_time, finish_time))
+
+        current_time = finish_time
+
+    return results, timeline
